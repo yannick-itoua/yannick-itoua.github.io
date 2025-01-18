fa37a38d700e60345474e63ce32e2e5579719a1a
Yannick Mounguengui <angeyannick.mounguenguiitoua.etu@univ-lille.fr>
2025-01-17 21:45:53 -0700

    ok

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,69 +1,58 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Slider from "react-slick";
-import { Card, CardContent, CardActions, Typography, Button, CardMedia } from "@mui/material";
-//nothing
+import { Card, CardContent, CardActions, Typography, Grid, Button, CardMedia } from "@mui/material";
+
 const ProjectsPage = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://portfolio-backend-tkqm.onrender.com/api/projects") // Backend URL
+      .get("https://portfolio-backend-tkqm.onrender.com/api/projects") // URL du backend déployé
       .then((response) => setProjects(response.data))
       .catch((error) => console.error("Error fetching projects:", error));
   }, []);
 
-  // Settings for the carousel
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
   return (
     <div style={{ padding: "20px" }}>
       <Typography variant="h4" gutterBottom style={{ textAlign: "center", marginBottom: "20px" }}>
         My Projects
       </Typography>
-      <Slider {...sliderSettings}>
+      <Grid container spacing={4}>
         {projects.map((project) => (
-          <div key={project._id}>
+          <Grid item xs={12} sm={6} md={4} key={project._id}>
             <Card
               sx={{
-                maxWidth: 500,
-                margin: "0 auto",
                 ":hover": {
-                  transform: "scale(1.05)",
+                  transform: "scale(1.02)",
                   boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.3)",
                 },
                 transition: "all 0.3s ease-in-out",
               }}
             >
-              {/* Image Section */}
+              {/* Image */}
               <CardMedia
                 component="img"
                 height="200"
-                image={project.image} // Path to the image
+                image={project.image} // Chemin de l'image
                 alt={project.title}
               />
+              {/* Titre et description */}
               <CardContent>
                 <Typography variant="h5">{project.title}</Typography>
                 <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
                   {project.description}
                 </Typography>
               </CardContent>
+              {/* Boutons */}
               <CardActions>
                 <Button size="small" color="primary" href={project.githubLink} target="_blank">
                   GitHub
                 </Button>
               </CardActions>
             </Card>
-          </div>
+          </Grid>
         ))}
-      </Slider>
+      </Grid>
     </div>
   );
 };
