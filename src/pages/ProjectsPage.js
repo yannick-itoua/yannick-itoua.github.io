30d091cac93f0d2dbe342a2ee1ca984d3322f0bc
yannick-itoua <mangeyannick@gmail.com>
2025-06-03 16:45:39 -0600

    add 2 projects

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -14,7 +14,7 @@ const ProjectsPage = () => {
       period: "Apr 2024 - Jul 2024 · 4 mos",
       location: "Lille, Hauts-de-France, France · Hybrid",
       description: "During my internship at CGI, I took on meaningful responsibilities in backend development where I built and maintained REST APIs that seamlessly connected critical systems. I gained valuable experience working with enterprise tools like Intersystems Cache, while using Postman, DBeaver, and Swagger to ensure API reliability and data integrity. Beyond coding, I actively participated in an Agile team environment, contributing to architectural discussions and using data insights to drive performance improvements. I also enhanced deployment workflows using Azure CI/CD pipelines, significantly reducing release times. One of my key contributions was resolving complex support tickets that improved client satisfaction and taught me how to balance technical problem-solving with effective communication in a professional consulting environment.",
-      image: "/cgi-internship.png",
+      
       skills: ["Intersystems Cache", "Postman API", "DBeaver", "Mockoon", "SQL", "ObjectScript", "Agile Methodologies", "Microsoft Azure", "Visual Studio", "REST APIs", "Filezilla", "Swagger API", "Docker"]
     }
   ]);
